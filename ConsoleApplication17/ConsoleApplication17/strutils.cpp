1ee1f8633b02e77d0141827011d9a8f03eb8c429
yosha28 <56972122+yosha28@users.noreply.github.com>
2019-10-29 16:49:24 +0200

    zy
    zy

--- ConsoleApplication17/ConsoleApplication17/strutils.cpp
+++ ConsoleApplication17/ConsoleApplication17/strutils.cpp
@@ -16,7 +16,7 @@ char *parseItem(char *fromChar, const char delimiter, char *parsedItem) {
 	char *result = NULL;
 	if (NULL != fromChar) {
 		
-		result = strchr(fromChar, &delimiter);
+		result = strstr(fromChar, &delimiter);
 		int res = 0;
 		if (NULL != result) {
 			for (int i = 0; i < (result - fromChar); i++) {
